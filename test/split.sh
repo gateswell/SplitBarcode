53cdfe1616c8d3f8e0eddcbc887f63f73ea910eb
gateswell <caoshuhuan@yeah.net>
2019-04-29 11:40:19 +0800

    delete split.sh

--- test/split.sh
+++
@@ -1,3 +0,0 @@
-echo ============ start at `date` ==============
-perl SplitBarcode.pl -r1 ./test_1.fq.gz -r2 ./test_2.fq.gz -f 101 -l 110 -b ./bc.list  -c Y -rc N
-echo ============ end at `date` ==============
